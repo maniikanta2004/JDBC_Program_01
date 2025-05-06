ae1f511e8e7669b1c8a0f3c3c12f02fd7056cf25
maniikanta2004 <bmanikanta597@gmail.com>
2025-05-06 17:39:32 +0530

    Update Program_01.java

--- src/com/test/Program_01.java
+++ src/com/test/Program_01.java
@@ -1,9 +1,13 @@
 package com.test;
-
+import java.util.Scanner;
 public class Program_01 {
 
 	public static void main(String[] args) {
-		System.out.printl("Hello World")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter n Value: ");
+		int n = sc.nextInt();
+		System.out.print("Square of "+n+" is "+(n*n));
+		sc.close();
 
 	}
 
